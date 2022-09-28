eb66f57ffaf035823c00a1841853244ecc97f9bd
Jaspreet Khela <jaspreetkhela@Jaspreets-MBP.ht.home>
2022-09-28 18:56:45 -0400

    Finished cleaning the datasets and exporting them to the PostgreSQL movie_data database

---
+++ data/count_data.sql
@@ -0,0 +1,5 @@
+-- Count the number of rows from the movies table
+SELECT COUNT(*) AS movies_rows FROM movies;
+
+-- Count the number of rows from the ratings table
+--SELECT COUNT(*) AS ratings_rows FROM ratings;
